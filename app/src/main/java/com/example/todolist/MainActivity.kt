804982b464618b43158b0bd39e3a857431fafe80
colussicode <colussidev@gmail.com>
2023-03-13 16:35:01 -0300

    fixing layout problems by changing viewbinding to findviewbyid

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.databinding.ActivityMainBinding
-import com.example.todolist.databinding.TodoItemBinding
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.todolist
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.TodoItemBinding
 
@@ -16,23 +18,26 @@ class TodoAdapter(
         notifyDataSetChanged()
     }
 
-    class ViewHolder(binding: TodoItemBinding): RecyclerView.ViewHolder(binding.root) {
-        var itemBinding = binding
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val checkBox: CheckBox = view.findViewById(R.id.todo_checkbox)
     }
 
+    //TODO: Can't use ViewBinding here (Loosing style properties)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = TodoItemBinding.inflate(LayoutInflater.from(parent.context))
-        return ViewHolder(binding)
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_item, parent, false)
+
+        return ViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = todoList[position]
 
-        holder.itemBinding.todoCheckbox.text = item.todoTitle
-        if(holder.itemBinding.todoCheckbox.isChecked) {
-            holder.itemBinding.todoCheckbox.isChecked = false
+        holder.checkBox.text = item.todoTitle
+        if(holder.checkBox.isChecked) {
+            holder.checkBox.isChecked = false
         }
-        holder.itemBinding.todoCheckbox.setOnClickListener {
+        holder.checkBox.setOnClickListener {
             onRemoveTodo(item.todoTitle)
         }
     }
